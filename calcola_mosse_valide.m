639feaf0ab342ca2ac798b66060a817ab60217b1
andreatrdt <75367731+andreatrdt@users.noreply.github.com>
2024-10-04 14:31:51 +0200

    movimento aggiornato

---
+++ calcola_mosse_valide.m
@@ -0,0 +1,21 @@
+function valid_moves = calcola_mosse_valide(row, col, size_table, M)
+    % Calcola le mosse valide per un pezzo nella posizione (row, col)
+    valid_moves = [];
+
+    % Definisci i possibili spostamenti (8 direzioni: su, giù, sinistra, destra, diagonali)
+    directions = [-1, -1; -1, 0; -1, 1; 0, -1; 0, 1; 1, -1; 1, 0; 1, 1];
+    
+    % Loop su tutte le direzioni
+    for i = 1:size(directions, 1)
+        newRow = row + directions(i, 1);
+        newCol = col + directions(i, 2);
+
+        % Verifica che la mossa sia all'interno della scacchiera
+        if newRow >= 1 && newRow <= size_table && newCol >= 1 && newCol <= size_table
+            % Verifica che la casella sia vuota o contenga un pezzo avversario
+            %if M(size_table - newRow + 1, newCol) == 0  % Casella vuota
+            valid_moves = [valid_moves; newRow, newCol];
+            % end
+        end
+    end
+end
--- main_Assalto_reale.m
+++ main_Assalto_reale.m
@@ -4,11 +4,11 @@ clear all
 close all
 
 % Variabili globali per tenere traccia della selezione
-global selectedRow selectedCol selectedColor flag punti;
+global selectedRow selectedCol selectedColor flag punti turn;
 
 
 punti = [0 ,0];
-
+turn = 0;
 
 % Set the seed for the random number generator
 rng(42)
@@ -17,16 +17,21 @@ tic
 % Definisci la dimensione della matrice
 size_table = 14; 
 
-% Genera la matrice
-M = generate_table(size_table);
 
 % Definisci il numero di caselle speciali
 N_special = 3; 
 
 N = zeros(size_table,size_table);
 
+%% MAIN
+
+% Genera la matrice
+M = generate_table(size_table);
+
+% genera caselle speciali
 [N,M] = cerca_casella(size_table,N,M,N_special);
 
+% genera scacchiera
 createChessBoard(size_table,N,M)
 
 
